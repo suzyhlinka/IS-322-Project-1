8e2dfdfcf614a8c3e2d03f15e1fd877894aee9a5
Suzy Hlinka <snh7@njit.edu>
2020-02-19 04:14:40 -0500

    got images, started productlist js and and html

---
+++ prodList.js
@@ -0,0 +1,59 @@
+var beesWax = new Object();
+beesWax.name = "Bees Wax Paper";
+beesWax.price = "$6.99";
+beesWax.category = "Cleaning & Storage";
+beesWax.url = "imgs/beeswax.jpg";
+
+var produceBags = new Object();
+produceBags.name = "Produce Bags";
+produceBags.price = "$7.99";
+produceBags.category = "Cleaning & Storage";
+produceBags.url = "imgs/producebags.jpg";
+
+var masonJar = new Object();
+masonJar.name = "Mason Jar";
+masonJar.price = "$1.99";
+masonJar.category = "Cleaning & Storage";
+masonJar.url = "imgs/masonjar.jpg";
+
+var sprayBottle = new Object();
+sprayBottle.name = "Glass Spray Bottle";
+sprayBottle.price = "$5.99";
+sprayBottle.category = "Cleaning & Storage";
+sprayBottle.url = "imgs/spraybottle.jpg";
+
+var dishBlock = new Object();
+dishBlock.name = "Dish Washing Block";
+dishBlock.price = "$9.99";
+dishBlock.category = "Cleaning & Storage";
+dishBlock.url = "imgs/dishblock.jpg";
+
+var shampoo = new Object();
+shampoo.name = "Shampoo Bar";
+shampoo.price = "$3.99";
+shampoo.category = "Health & Beauty";
+shampoo.url = "imgs/shampoo.jpg";
+
+var toothpaste = new Object();
+toothpaste.name = "Toothpaste Bites";
+toothpaste.price = "$8.99";
+toothpaste.category = "Health & Beauty";
+toothpaste.url = "imgs/toothpaste.jpg";
+
+var toothbrush = new Object();
+toothbrush.name = "Bamboo Toothbrush";
+toothbrush.price = "$2.99";
+toothbrush.category = "Health & Beauty";
+toothbrush.url = "imgs/toothbrush.jpg";
+
+var deoderant = new Object();
+deoderant.name = "Zero Waste Deoderant";
+deoderant.price = "$4.99";
+deoderant.category = "Health & Beauty";
+deoderant.url = "imgs/deoderant.jpg";
+
+var mascara = new Object();
+mascara.name = "Zero Waste Mascara";
+mascara.price = "$10.99";
+mascara.category = "Health & Beauty";
+mascara.url = "imgs/mascara.jpg";
