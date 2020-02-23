7b71ce9bdf639001c8adcaf39ffbd2fc31f32e80
Suzy Hlinka <snh7@njit.edu>
2020-02-23 11:37:17 -0500

    created product description page and js

---
+++ productPage.js
@@ -0,0 +1,77 @@
+var beesWax = new Object();
+beesWax.name = "Bees Wax Paper";
+beesWax.price = "6.99";
+beesWax.category = "Cleaning & Storage";
+beesWax.url = "imgs/beeswax.jpg";
+beesWax.desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel felis sed diam condimentum efficitur in eget tellus. Fusce tincidunt interdum massa, in fermentum leo tincidunt vitae. Maecenas sagittis varius metus id consectetur. Nulla condimentum, quam mollis laoreet rutrum, neque odio congue diam, sed pulvinar lectus mauris eget massa. Curabitur interdum convallis sem at pellentesque.";
+
+var produceBags = new Object();
+produceBags.name = "Produce Bags";
+produceBags.price = "7.99";
+produceBags.category = "Cleaning & Storage";
+produceBags.url = "imgs/producebags.jpg";
+produceBags.desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel felis sed diam condimentum efficitur in eget tellus. Fusce tincidunt interdum massa, in fermentum leo tincidunt vitae. Maecenas sagittis varius metus id consectetur. Nulla condimentum, quam mollis laoreet rutrum, neque odio congue diam, sed pulvinar lectus mauris eget massa. Curabitur interdum convallis sem at pellentesque.";
+
+var masonJar = new Object();
+masonJar.name = "Mason Jar";
+masonJar.price = "1.99";
+masonJar.category = "Cleaning & Storage";
+masonJar.url = "imgs/masonjar.jpg";
+masonJar.desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel felis sed diam condimentum efficitur in eget tellus. Fusce tincidunt interdum massa, in fermentum leo tincidunt vitae. Maecenas sagittis varius metus id consectetur. Nulla condimentum, quam mollis laoreet rutrum, neque odio congue diam, sed pulvinar lectus mauris eget massa. Curabitur interdum convallis sem at pellentesque.";
+
+var sprayBottle = new Object();
+sprayBottle.name = "Glass Spray Bottle";
+sprayBottle.price = "5.99";
+sprayBottle.category = "Cleaning & Storage";
+sprayBottle.url = "imgs/spraybottle.jpg";
+sprayBottle.desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel felis sed diam condimentum efficitur in eget tellus. Fusce tincidunt interdum massa, in fermentum leo tincidunt vitae. Maecenas sagittis varius metus id consectetur. Nulla condimentum, quam mollis laoreet rutrum, neque odio congue diam, sed pulvinar lectus mauris eget massa. Curabitur interdum convallis sem at pellentesque.";
+
+var dishBlock = new Object();
+dishBlock.name = "Dish Washing Block";
+dishBlock.price = "9.99";
+dishBlock.category = "Cleaning & Storage";
+dishBlock.url = "imgs/dishblock.jpg";
+dishBlock.desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel felis sed diam condimentum efficitur in eget tellus. Fusce tincidunt interdum massa, in fermentum leo tincidunt vitae. Maecenas sagittis varius metus id consectetur. Nulla condimentum, quam mollis laoreet rutrum, neque odio congue diam, sed pulvinar lectus mauris eget massa. Curabitur interdum convallis sem at pellentesque.";
+
+var shampoo = new Object();
+shampoo.name = "Shampoo Bar";
+shampoo.price = "3.99";
+shampoo.category = "Health & Beauty";
+shampoo.url = "imgs/shampoo.jpg";
+shampoo.desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel felis sed diam condimentum efficitur in eget tellus. Fusce tincidunt interdum massa, in fermentum leo tincidunt vitae. Maecenas sagittis varius metus id consectetur. Nulla condimentum, quam mollis laoreet rutrum, neque odio congue diam, sed pulvinar lectus mauris eget massa. Curabitur interdum convallis sem at pellentesque.";
+
+var toothpaste = new Object();
+toothpaste.name = "Toothpaste Bites";
+toothpaste.price = "8.99";
+toothpaste.category = "Health & Beauty";
+toothpaste.url = "imgs/toothpaste.jpg";
+toothpaste.desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel felis sed diam condimentum efficitur in eget tellus. Fusce tincidunt interdum massa, in fermentum leo tincidunt vitae. Maecenas sagittis varius metus id consectetur. Nulla condimentum, quam mollis laoreet rutrum, neque odio congue diam, sed pulvinar lectus mauris eget massa. Curabitur interdum convallis sem at pellentesque.";
+
+var toothbrush = new Object();
+toothbrush.name = "Bamboo Toothbrush";
+toothbrush.price = "2.99";
+toothbrush.category = "Health & Beauty";
+toothbrush.url = "imgs/toothbrush.jpg";
+toothbrush.desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel felis sed diam condimentum efficitur in eget tellus. Fusce tincidunt interdum massa, in fermentum leo tincidunt vitae. Maecenas sagittis varius metus id consectetur. Nulla condimentum, quam mollis laoreet rutrum, neque odio congue diam, sed pulvinar lectus mauris eget massa. Curabitur interdum convallis sem at pellentesque.";
+
+var deoderant = new Object();
+deoderant.name = "Zero Waste Deoderant";
+deoderant.price = "4.99";
+deoderant.category = "Health & Beauty";
+deoderant.url = "imgs/deoderant.jpg";
+deoderant.desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel felis sed diam condimentum efficitur in eget tellus. Fusce tincidunt interdum massa, in fermentum leo tincidunt vitae. Maecenas sagittis varius metus id consectetur. Nulla condimentum, quam mollis laoreet rutrum, neque odio congue diam, sed pulvinar lectus mauris eget massa. Curabitur interdum convallis sem at pellentesque.";
+
+var mascara = new Object();
+mascara.name = "Zero Waste Mascara";
+mascara.price = "10.99";
+mascara.category = "Health & Beauty";
+mascara.url = "imgs/mascara.jpg";
+mascara.desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel felis sed diam condimentum efficitur in eget tellus. Fusce tincidunt interdum massa, in fermentum leo tincidunt vitae. Maecenas sagittis varius metus id consectetur. Nulla condimentum, quam mollis laoreet rutrum, neque odio congue diam, sed pulvinar lectus mauris eget massa. Curabitur interdum convallis sem at pellentesque.";
+
+var items = [beesWax, produceBags, masonJar, sprayBottle, dishBlock, shampoo, toothpaste, toothbrush, deoderant, mascara];
+
+var container = document.getElementById("details-contain");
+
+randomNumber = Math.floor(Math.random() * Math.floor(items.length));
+
+container.innerHTML = "<div class='product-image'><img src='" + items[randomNumber].url + "'></div><div class='product-details'><h2>" + items[randomNumber].name + "</h2><h3>" + items[randomNumber].price + "</h3><p>" + items[randomNumber].desc + "</p><button>Add to Cart</button></div>";
